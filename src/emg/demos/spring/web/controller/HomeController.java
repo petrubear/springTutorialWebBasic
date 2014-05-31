52a5998383573fc0d69cf92ac004f9ae38f03205
Edison Martinez <edison.martinez@me.com>
2014-05-30 23:14:50 -0500

    add app log appender

--- src/emg/demos/spring/web/controller/HomeController.java
+++ src/emg/demos/spring/web/controller/HomeController.java
@@ -1,14 +1,18 @@
 package emg.demos.spring.web.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+	private static Logger logger = Logger.getLogger(HomeController.class);
+
 	@RequestMapping("/")
 	public String showHome(Model model) {
-
+		logger.debug("Showing Home Page");
 		return "home";
 	}
 
